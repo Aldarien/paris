599036f91e9a7dbbd3842fd88a9da46815b37537
Aldarien <aldarien85@gmail.com>
2018-08-09 19:26:25 -0400

    v0.1.0

---
+++ app/Service/ParisConfig.php
@@ -0,0 +1,53 @@
+<?php
+namespace App\Service;
+
+use \App\Alias\Model;
+
+class ParisConfig
+{
+  protected $configuration;
+
+  public function __construct($configuration = null)
+  {
+    if ($configuration == null) {
+      $configuration = [];
+      $configuration['default'] = 'mysql';
+    }
+    $this->configuration = $configuration;
+  }
+  public function setUp($databases)
+  {
+    $this->set($databases[$this->configuration['default']]);
+    unset($databases[$this->configuration['default']]);
+    if (count($databases) > 0) {
+      foreach ($databases as $name => $data) {
+        $this->set($data);
+      }
+    }
+  }
+  protected function set($data)
+  {
+    switch ($data['engine']) {
+      case 'mysql':
+        $this->setMySQL($data);
+        break;
+      case 'sqlite':
+        $this->setSQLite($data);
+        break;
+    }
+  }
+  protected function setMySQL($data)
+  {
+    $dsn = 'mysql:host=' . $data['host']['name'];
+    if (isset($data['host']['port'])) {
+      $dsn .= ';port=' . $data['host']['port'];
+    }
+    $dsn .= ';dbname=' . $data['name'];
+    \ORM::configure($dsn);
+    \ORM::configure('username', $data['user']['name']);
+    \ORM::configure('password', $data['user']['password']);
+    if (isset($data['short_names']) and $data['short_names']) {
+      Model::$short_table_names = true;
+    }
+  }
+}
